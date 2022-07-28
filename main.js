343d8fa9182302c0a2212ecd3a37fd656a04806e
nafiul01 <nafiul01@gmail.com>
2022-07-29 01:34:24 +0600

    Random question with 40% new question when restart the quiz
    1. Total 20 question.
    2. Random question will show up.
    3. When restart the quiz 60% question will be common from the last quiz and 40% question will be new.

--- main.js
+++ main.js
@@ -59,6 +59,186 @@ let questions = [
             "Center Power Unit",
             "Central Processing Unit",
         ]
+    },
+
+    {
+        num: 6,
+        question:"Who is the father of Computer?",
+        answer:"Charles Babbage",
+        options:[
+            "Allen Turing",
+            "Edward Robert",
+            "Charles Babbage",
+            "Albert Einstein",
+        ]
+    },
+
+    {
+        num: 7,
+        question:"What does CSS stand for?",
+        answer:"Cascading Style Sheets",
+        options:[
+            "CSS Style Sheet",
+            "Cascading Style Sheets",
+            "Complete Style Sheet",
+            "Comparative Style Sheet",
+        ]
+    },
+
+    {
+        num: 8,
+        question:"Full Meaning of HTML",
+        answer:"Hypertext Markup Language",
+        options:[
+            "Hypertext Makeup Language",
+            "Hydrotext Markup Language",
+            "Hypertext Markup Language",
+            "Hopetext Makeup Language",
+        ]
+    },
+
+    {
+        num: 9,
+        question:"Which one is programming language ?",
+        answer:"js",
+        options:[
+            "js",
+            "English",
+            "Hindi",
+            "Urdu",
+        ]
+    },
+
+    {
+        num: 10,
+        question:"Full meaning of CPU",
+        answer:"Central Processing Unit",
+        options:[
+            "Central Power Unit",
+            "Central Processing Utility",
+            "Center Power Unit",
+            "Central Processing Unit",
+        ]
+    },
+
+    {
+        num: 11,
+        question:"Who is the father of Computer?",
+        answer:"Charles Babbage",
+        options:[
+            "Allen Turing",
+            "Edward Robert",
+            "Charles Babbage",
+            "Albert Einstein",
+        ]
+    },
+
+    {
+        num: 12,
+        question:"What does CSS stand for?",
+        answer:"Cascading Style Sheets",
+        options:[
+            "CSS Style Sheet",
+            "Cascading Style Sheets",
+            "Complete Style Sheet",
+            "Comparative Style Sheet",
+        ]
+    },
+
+    {
+        num: 13,
+        question:"Full Meaning of HTML",
+        answer:"Hypertext Markup Language",
+        options:[
+            "Hypertext Makeup Language",
+            "Hydrotext Markup Language",
+            "Hypertext Markup Language",
+            "Hopetext Makeup Language",
+        ]
+    },
+
+    {
+        num: 14,
+        question:"Which one is programming language ?",
+        answer:"js",
+        options:[
+            "js",
+            "English",
+            "Hindi",
+            "Urdu",
+        ]
+    },
+
+    {
+        num: 15,
+        question:"Full meaning of CPU",
+        answer:"Central Processing Unit",
+        options:[
+            "Central Power Unit",
+            "Central Processing Utility",
+            "Center Power Unit",
+            "Central Processing Unit",
+        ]
+    },
+
+    {
+        num: 16,
+        question:"Who is the father of Computer?",
+        answer:"Charles Babbage",
+        options:[
+            "Allen Turing",
+            "Edward Robert",
+            "Charles Babbage",
+            "Albert Einstein",
+        ]
+    },
+
+    {
+        num: 17,
+        question:"What does CSS stand for?",
+        answer:"Cascading Style Sheets",
+        options:[
+            "CSS Style Sheet",
+            "Cascading Style Sheets",
+            "Complete Style Sheet",
+            "Comparative Style Sheet",
+        ]
+    },
+
+    {
+        num: 18,
+        question:"Full Meaning of HTML",
+        answer:"Hypertext Markup Language",
+        options:[
+            "Hypertext Makeup Language",
+            "Hydrotext Markup Language",
+            "Hypertext Markup Language",
+            "Hopetext Makeup Language",
+        ]
+    },
+
+    {
+        num: 19,
+        question:"Which one is programming language ?",
+        answer:"js",
+        options:[
+            "js",
+            "English",
+            "Hindi",
+            "Urdu",
+        ]
+    },
+
+    {
+        num: 20,
+        question:"Full meaning of CPU",
+        answer:"Central Processing Unit",
+        options:[
+            "Central Power Unit",
+            "Central Processing Utility",
+            "Center Power Unit",
+            "Central Processing Unit",
+        ]
     }
 ]
 
@@ -80,6 +260,11 @@ const nextBtn = document.getElementById("next");
 const restartBtn = document.getElementById("restart");
 
 
+let counter=0;
+let numArray = [];
+let userScore=0;
+let totalQuestion = 5;
+
 
 // Intro Page
 
@@ -96,18 +281,27 @@ goBackBtn.onclick = function () {
     rulesPage.classList.add("hidden");
 }
 
-let counter=0;
 
 startNowBtn.onclick = function () {
     rulesPage.classList.add("hidden");
     questionPage.classList.remove("hidden");
-    questionCounter(counter);
+    
+    for (let i=0; numArray.length<totalQuestion; i++){
+        let randomNumber = Math.floor(Math.random()*20);
+        numArray.push(randomNumber);
+        numArray = [...new Set(numArray)];
+    }
+
+    console.log("Start "+numArray);
+    
+    questionCounter(numArray[counter]);
 }
 
 // Question Page
 
 function questionCounter(index){
     questionNo.innerHTML = questions[index].num;
+    //questionNo.innerHTML = counter+1;
     questionHTML.innerHTML = questions[index].question;
     questionOption.innerHTML = '<div class="btn-option"><span>' + questions[index].options[0] + '</span></div>' 
     + '<div class="btn-option"><span>' + questions[index].options[1] + '</span></div>'
@@ -119,8 +313,6 @@ function questionCounter(index){
         option[i].setAttribute("onclick", "optionSelected(this)"); 
     }
 }
-
-let userScore=0;
 
 function optionSelected(answer){
     let userAns = answer.textContent;
@@ -145,9 +337,9 @@ function optionSelected(answer){
 
 
 nextBtn.onclick = function(){
-    if (counter<questions.length-1){
+    if (counter<totalQuestion-1){
         counter++;
-        questionCounter(counter);
+        questionCounter(numArray[counter]);
     }
     else {
         questionPage.classList.add("hidden");
@@ -163,5 +355,26 @@ restartBtn.onclick = function(){
     introPage.classList.remove("hidden");
     result.classList.add("hidden");
     userScore=0;
+    //numArray.sort();
+    numArray = numArray.sort((a, b) => 0.5 - Math.random());
+    numArray.shift();
+    numArray.shift();
+    console.log("End "+numArray);
+}
+
+
+// Random Number
+
+const random = document.getElementById("random-number");
+const displayRandom = document.getElementById("number");
+
+random.onclick = function (){
+    let numArray=[];
+    for (let i=0; numArray.length<totalQuestion; i++){
+        let randomNumber = Math.floor(Math.random()*20);
+        numArray.push(randomNumber);
+        numArray = [...new Set(numArray)];
+    }    
+    console.log(numArray);
 }
 
